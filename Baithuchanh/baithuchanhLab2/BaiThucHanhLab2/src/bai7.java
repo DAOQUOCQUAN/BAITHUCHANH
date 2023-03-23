7cca97270ffb17ac0194827b459e6dd14c6e96ce
DAOQQUAN <qdao8189@gamil.com>
2023-03-23 12:27:18 +0700

    first commit

--- Baithuchanh/baithuchanhLab2/BaiThucHanhLab2/src/bai7.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class bai7 {
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 20; i++)
-            System.out.println("20 so nguyen duong dau tien la: " + i);
-    }
-}
